66125b660f67f03a60a9319055d309186a21ce5d
Adrian Gadient <adrian.gadient@ag.ch>
2024-09-10 14:31:37 +0000

    tests added for all px tables

--- tests/testthat/helper-api.R
+++ tests/testthat/helper-api.R
@@ -1,4 +1,6 @@
-stattab_101_snap <-  BFS::bfs_get_metadata(number_bfs = "px-x-0102010000_101")
+# px-x-0104020000_101
+stattab_101_snap <-  BFS::bfs_get_metadata(
+  number_bfs = "px-x-0104020000_101")
 stattab_101_snap <- stattab_101_snap |>
   dplyr::select(-valueTexts) |>
   tidyr::unnest_longer(values) |>
@@ -9,3 +11,77 @@ stattab_101_snap <- stattab_101_snap |>
       dplyr::pull(valueTexts)
   ) |>
   dplyr::select(code, text, values, valueTexts, everything())
+
+# px-x-0104020000_102
+stattab_102_snap <-  BFS::bfs_get_metadata(
+  number_bfs = "px-x-0104020000_102")
+stattab_102_snap <- stattab_102_snap |>
+  dplyr::select(-valueTexts) |>
+  tidyr::unnest_longer(values) |>
+  dplyr::mutate(
+    valueTexts = stattab_102_snap |>
+      dplyr::select(valueTexts) |>
+      tidyr::unnest_longer(valueTexts) |>
+      dplyr::pull(valueTexts)
+  ) |>
+  dplyr::select(code, text, values, valueTexts, everything())
+
+# px-x-0104020000_103
+stattab_103_snap <-  BFS::bfs_get_metadata(
+  number_bfs = "px-x-0104020000_103")
+stattab_103_snap <- stattab_103_snap |>
+  dplyr::select(-valueTexts) |>
+  tidyr::unnest_longer(values) |>
+  dplyr::mutate(
+    valueTexts = stattab_103_snap |>
+      dplyr::select(valueTexts) |>
+      tidyr::unnest_longer(valueTexts) |>
+      dplyr::pull(valueTexts)
+  ) |>
+  dplyr::select(code, text, values, valueTexts, everything())
+
+# px-x-0104020000_106
+
+stattab_106_snap <- BFS::bfs_get_metadata(
+  number_bfs = "px-x-0104020000_106")
+stattab_106_snap <- stattab_106_snap |>
+  dplyr::select(-valueTexts) |>
+  tidyr::unnest_longer(values) |>
+  dplyr::mutate(
+    valueTexts = stattab_106_snap |>
+      dplyr::select(valueTexts) |>
+      tidyr::unnest_longer(valueTexts) |>
+      dplyr::pull(valueTexts)
+  ) |>
+  dplyr::select(code, text, values, valueTexts, everything())
+
+
+# px-x-0104020000_109
+stattab_109_snap <- BFS::bfs_get_metadata(
+  number_bfs = "px-x-0104020000_109")
+stattab_109_snap <- stattab_109_snap |>
+  dplyr::select(-valueTexts) |>
+  tidyr::unnest_longer(values) |>
+  dplyr::mutate(
+    valueTexts = stattab_109_snap |>
+      dplyr::select(valueTexts) |>
+      tidyr::unnest_longer(valueTexts) |>
+      dplyr::pull(valueTexts)
+  ) |>
+  dplyr::select(code, text, values, valueTexts, everything())
+
+# px-x-0102010000_101
+stattab_pop_snap <- BFS::bfs_get_metadata(
+  number_bfs = "px-x-0102010000_101")
+stattab_pop_snap <- stattab_pop_snap |>
+  dplyr::select(-valueTexts) |>
+  tidyr::unnest_longer(values) |>
+  dplyr::mutate(
+    valueTexts = stattab_pop_snap |>
+      dplyr::select(valueTexts) |>
+      tidyr::unnest_longer(valueTexts) |>
+      dplyr::pull(valueTexts)
+  ) |>
+  dplyr::select(code, text, values, valueTexts, everything())
+
+
--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,10 +1,11 @@
-test_that("snapshot table px-x-0102010000_101", {
+test_that("snapshot table px-x-0104020000_101", {
 
   # don't run on gitlab ci
   skip_on_ci()
 
-  stattab_101 <-  BFS::bfs_get_metadata(number_bfs = "px-x-0102010000_101")
-  stattab_101 <- stattab_101|>
+  stattab_101 <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0104020000_101")
+  stattab_101 <- stattab_101 |>
     dplyr::select(-valueTexts) |>
     tidyr::unnest_longer(values) |>
     dplyr::mutate(
@@ -19,3 +20,117 @@ test_that("snapshot table px-x-0102010000_101", {
 
 })
 
+test_that("snapshot table px-x-0104020000_102", {
+
+  # don't run on gitlab ci
+  skip_on_ci()
+
+  stattab_102 <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0104020000_102")
+  stattab_102 <- stattab_102 |>
+    dplyr::select(-valueTexts) |>
+    tidyr::unnest_longer(values) |>
+    dplyr::mutate(
+      valueTexts = stattab_102 |>
+        dplyr::select(valueTexts) |>
+        tidyr::unnest_longer(valueTexts) |>
+        dplyr::pull(valueTexts)
+    ) |>
+    dplyr::select(code, text, values, valueTexts, everything())
+
+  expect_identical(stattab_102, stattab_102_snap)
+
+})
+
+
+
+test_that("snapshot table px-x-0104020000_103", {
+
+  # don't run on gitlab ci
+  skip_on_ci()
+
+  stattab_103 <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0104020000_103")
+  stattab_103 <- stattab_103 |>
+    dplyr::select(-valueTexts) |>
+    tidyr::unnest_longer(values) |>
+    dplyr::mutate(
+      valueTexts = stattab_103 |>
+        dplyr::select(valueTexts) |>
+        tidyr::unnest_longer(valueTexts) |>
+        dplyr::pull(valueTexts)
+    ) |>
+    dplyr::select(code, text, values, valueTexts, everything())
+
+  expect_identical(stattab_103, stattab_103_snap)
+
+})
+
+
+test_that("snapshot table px-x-0104020000_106", {
+
+  # don't run on gitlab ci
+  skip_on_ci()
+
+  stattab_106 <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0104020000_106")
+  stattab_106 <- stattab_106 |>
+    dplyr::select(-valueTexts) |>
+    tidyr::unnest_longer(values) |>
+    dplyr::mutate(
+      valueTexts = stattab_106 |>
+        dplyr::select(valueTexts) |>
+        tidyr::unnest_longer(valueTexts) |>
+        dplyr::pull(valueTexts)
+    ) |>
+    dplyr::select(code, text, values, valueTexts, everything())
+
+  expect_identical(stattab_106, stattab_106_snap)
+
+})
+
+
+test_that("snapshot table px-x-0104020000_109", {
+
+  # don't run on gitlab ci
+  skip_on_ci()
+
+  stattab_109 <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0104020000_109")
+  stattab_109 <- stattab_109 |>
+    dplyr::select(-valueTexts) |>
+    tidyr::unnest_longer(values) |>
+    dplyr::mutate(
+      valueTexts = stattab_109 |>
+        dplyr::select(valueTexts) |>
+        tidyr::unnest_longer(valueTexts) |>
+        dplyr::pull(valueTexts)
+    ) |>
+    dplyr::select(code, text, values, valueTexts, everything())
+
+  expect_identical(stattab_109, stattab_109_snap)
+
+})
+
+
+test_that("snapshot table px-x-0102010000_101", {
+
+  # don't run on gitlab ci
+  skip_on_ci()
+
+  stattab_pop <-  BFS::bfs_get_metadata(
+    number_bfs = "px-x-0102010000_101")
+  stattab_pop <- stattab_pop |>
+    dplyr::select(-valueTexts) |>
+    tidyr::unnest_longer(values) |>
+    dplyr::mutate(
+      valueTexts = stattab_pop |>
+        dplyr::select(valueTexts) |>
+        tidyr::unnest_longer(valueTexts) |>
+        dplyr::pull(valueTexts)
+    ) |>
+    dplyr::select(code, text, values, valueTexts, everything())
+
+  expect_identical(stattab_pop, stattab_pop_snap)
+
+})
