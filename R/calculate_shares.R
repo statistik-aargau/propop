6075860d9c308e4f7872f2822a3d2332b89b8ab6
agar <Adrian.Gadient@ag.ch>
2024-12-20 14:48:56 +0000

    :recycle: :memo: updated documentation, changed some variable names

--- R/calculate_shares.R
+++ R/calculate_shares.R
@@ -1,9 +1,25 @@
-#' Calculate shares for distributing migration parameters to spatial units
+#' Calculate shares for distributing migration parameters among spatial units
 #'
-#' @param data_migration data frame, mutation data e.g. for international immigration or
-#' intercantonal immigration aggregated across demographic groups.
-#' @param column_migration string, name of the data column which contains the
+#' @param data_migration data frame, historical records (e.g., for immigration
+#' from other cantons or countries) aggregated across demographic groups.
+#' @param column_migration character, name of the column which contains the
 #' data for historical migration occurrences.
+#'
+#' @returns
+#' Returns the input data frame with the following new columns:
+#'
+#'* `age_group_10`: character, name of the 10-year age group to which the 1-year
+#'  age class is assigned to.
+#'* `sum_10`: numeric, total number of people in the 10-year age group.
+#'* `prop_10`: numeric, proportion of the the 10-year age group total that
+#'  is allocated to each 1-year age group.
+#'* `use_group`: character, preference for 1-year or 10-year age group. Defaults
+#'  to `age_group_1` if at least one observation was recorded.
+#'* `use_share`: numeric, share to be used according to `use_group`.
+#'* `use_share_sum`: numeric, total per demographic group and across all
+#'  spatial units.
+#'* `share`: numeric, the spatial unit's share relative to the total (across all
+#'  spatial units) of people within the same demographic group.
 #'
 #' @export
 #'
@@ -56,9 +72,9 @@ calculate_shares <- function(data_migration, column_migration) {
     # get the total and proportional total by coarser age groups
     mutate(
       # total number of people in 10-year age groups
-      sum_mig_10 = sum(!!sym(column_migration)),
+      sum_10 = sum(!!sym(column_migration)),
       # proportional total
-      prop_mig_10 = sum_mig_10 / 10,
+      prop_10 = sum_10 / 10,
       .by = any_of(c("nat", "sex", "spatial_unit", "age_group_10"))
     )
 
@@ -68,10 +84,10 @@ calculate_shares <- function(data_migration, column_migration) {
     mutate(
       use_group =
       # if there are observations in the single age group, use those
-        ifelse(!!sym(column_migration) != 0, "single_age_group",
+        ifelse(!!sym(column_migration) != 0, "age_group_1",
           # if there are no observations in the single age group, use the
           # proportional total of 10-year age groups
-          ifelse(!!sym(column_migration) == 0 & prop_mig_10 > 0, "age_group_10",
+          ifelse(!!sym(column_migration) == 0 & prop_10 > 0, "age_group_10",
             # if neither, single or 10-year age group have more than zero
             # observations, still use the 10-year age group (this happens rather
             # rarely; the share will be zero in those cases)
@@ -83,26 +99,26 @@ calculate_shares <- function(data_migration, column_migration) {
   # Check data for NAs in group choices ----
   assertthat::assert_that(isFALSE(NA %in% unique(df2$use_group)),
     msg = paste0("One or more observations could not be assigned to either",
-      "'single_age_group' or 'age_group_10'.")
+      "'age_group_1' or 'age_group_10'.")
   )
 
 # Calculate estimates for the number of migrations -----
   df3 <- df2 |>
     mutate(
       # get estimates depending on group choices
-      estimate_mig = case_when(
-        use_group == "single_age_group" ~ !!sym(column_migration),
-        use_group == "age_group_10" ~ prop_mig_10,
+      use_share = case_when(
+        use_group == "age_group_1" ~ !!sym(column_migration),
+        use_group == "age_group_10" ~ prop_10,
         TRUE ~ NA
       )
     ) |>
     # get the total by demographic group
-    mutate(estimate_mig_sum = sum(estimate_mig), .by = c(nat, sex, age)) |>
+    mutate(use_share_sum = sum(use_share), .by = c(nat, sex, age)) |>
     # calculate shares
-    mutate(share_migration = estimate_mig / estimate_mig_sum)
+    mutate(share = use_share / use_share_sum)
 
   # Check data for NAs in shares ----
-  assertthat::assert_that(isFALSE(NA %in% unique(df3$share_migration)),
+  assertthat::assert_that(isFALSE(NA %in% unique(df3$share)),
     msg = paste0("Shares for one or more observations are NA.")
   )
 
