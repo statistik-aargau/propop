2d158c98298ed78294cf502c7ca58e0be571cdee
nefo <norah.efosa@ag.ch>
2025-01-07 09:37:43 +0000

    :recycle: new argument for calculate_shares function

--- R/calculate_shares.R
+++ R/calculate_shares.R
@@ -4,18 +4,26 @@
 #' from other cantons or countries) aggregated across demographic groups.
 #' @param column_migration character, name of the column which contains the
 #' data for historical migration occurrences.
-#'
+#' @param age_group character, optional argument: age group used for
+#' calculating shares. If the argument is not specified, the default selects
+#' shares based on xyz. To use the same age clustering for all demographic units,
+#' the argument can be specified with "age_group_5" or "age_group_10".
 #' @returns
 #' Returns the input data frame with the following new columns:
 #'
+#'* `age_group_5`: character, name of the 5-year age group to which the 1-year
+#'  age class is assigned to.
+#'* `sum_5`: numeric, total number of people in the 5-year age group.
+#'* `prop_5`: numeric, proportion of the the 5-year age group total that
+#'  is allocated to each 1-year age group.
 #'* `age_group_10`: character, name of the 10-year age group to which the 1-year
 #'  age class is assigned to.
 #'* `sum_10`: numeric, total number of people in the 10-year age group.
 #'* `prop_10`: numeric, proportion of the the 10-year age group total that
 #'  is allocated to each 1-year age group.
-#'* `use_group`: character, preference for 1-year or 10-year age group. Defaults
-#'  to `age_group_1` if at least one observation was recorded.
-#'* `use_share`: numeric, share to be used according to `use_group`.
+#'* `use_age_group`: character, preference for 1-year, 5-year or 10-year age
+#'  group. Defaults to `age_group_1` if at least one observation was recorded.
+#'* `use_share`: numeric, share to be used according to `use_age_group`.
 #'* `use_share_sum`: numeric, total per demographic group and across all
 #'  spatial units.
 #'* `share`: numeric, the spatial unit's share relative to the total (across all
@@ -24,7 +32,11 @@
 #' @export
 #'
 #' @autoglobal
-calculate_shares <- function(data_migration, column_migration) {
+calculate_shares <- function(
+    data_migration,
+    column_migration,
+    age_group = "default"
+  ) {
   # Test input ----
   ## Presence of mandatory columns ----
   assertthat::assert_that("spatial_unit" %in% names(data_migration),
@@ -103,7 +115,7 @@ calculate_shares <- function(data_migration, column_migration) {
   df2 <- df1 |>
     # firstly, grouped by 5-year age groups
     mutate(
-      use_group = case_when(
+      use_age_group = case_when(
         # if all observations in 5-year age groups are zero, use 10-year age groups
         sum(sum_5) == 0 ~ "age_group_10",
         # if there are no observations in the single age group, use the
@@ -116,21 +128,21 @@ calculate_shares <- function(data_migration, column_migration) {
     ) |>
     # secondly, grouped by 10-year age groups
     mutate(
-      use_group = case_when(
+      use_age_group = case_when(
         # if the dummy variable includes "age_group_10" for any age group, use
         # the 10-year groups for all ages within those 10-year age groups.
-        "age_group_10" %in% use_group ~ "age_group_10",
+        "age_group_10" %in% use_age_group ~ "age_group_10",
         # if neither, single, 5-year or 10-year age groups have more than zero
         # observations, still use the 10-year age group (this happens rather
         # rarely; the share will be zero in those cases)
         sum(sum_10) == 0 ~ "age_group_10",
-        TRUE ~ use_group
+        TRUE ~ use_age_group
       ),
       .by = any_of(c("nat", "sex", "spatial_unit", "age_group_10"))
     )
 
   # Check data for NAs in group choices ----
-  assertthat::assert_that(isFALSE(NA %in% unique(df2$use_group)),
+  assertthat::assert_that(isFALSE(NA %in% unique(df2$use_age_group)),
     msg = paste0(
       "One or more observations could not be assigned to either",
       "'age_group_1', 'age_group_5' or 'age_group_10'."
@@ -138,25 +150,36 @@ calculate_shares <- function(data_migration, column_migration) {
   )
 
   # Calculate estimates for the number of migrations -----
+  if (age_group == "default") {
   df3 <- df2 |>
     mutate(
       # get estimates depending on group choices
-      use_share = case_when(
-        use_group == "age_group_1" ~ !!sym(column_migration),
-        use_group == "age_group_5" ~ prop_5,
-        use_group == "age_group_10" ~ prop_10,
+      n = case_when(
+        use_age_group == "age_group_1" ~ !!sym(column_migration),
+        use_age_group == "age_group_5" ~ prop_5,
+        use_age_group == "age_group_10" ~ prop_10,
         TRUE ~ NA
       )
-    ) |>
+    )
+  } else if (age_group == "age_group_5") {
+    df3 <- df2 |>
+      mutate(n = prop_5, use_age_group = "age_group_5")
+  } else if (age_group == "age_group_10") {
+    df3 <- df2 |>
+      mutate(n = prop_10, use_age_group = "age_group_10")
+  }
+
+  # Calculate shares
+  df4 <- df3 |>
     # get the total by demographic group
-    mutate(use_share_sum = sum(use_share), .by = c(nat, sex, age)) |>
+    mutate(n_sum = sum(n), .by = c(nat, sex, age)) |>
     # calculate shares
-    mutate(share = use_share / use_share_sum)
+    mutate(share = n / n_sum)
 
   # Check data for NAs in shares ----
-  assertthat::assert_that(isFALSE(NA %in% unique(df3$share)),
+  assertthat::assert_that(isFALSE(NA %in% unique(df4$share)),
     msg = paste0("Shares for one or more observations are NA.")
   )
 
-  return(df3)
+  return(df4)
 }
