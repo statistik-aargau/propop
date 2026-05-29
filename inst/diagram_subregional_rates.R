b956307eb15899fb661f6a95f84ddbed0950f034
Adrian Gadient <adrian.gadient@ag.ch>
2026-05-29 09:11:45 +0000

    updated figure for subregion vignette

--- inst/diagram_subregional_rates.R
+++ inst/diagram_subregional_rates.R
@@ -19,7 +19,7 @@ node [shape = rectangle, style = \"filled,rounded\", fillcolor = deepskyblue, fo
 data1 [label = 'Aggregated\npast migration', shape = folder, fillcolor = gold]
 # data2 [label = 'Dataset 2', shape = folder, fillcolor = gold]
 
-calc_emi [label =  'calculate\n_rate']
+calc_emi [label =  'calculate\n_rates']
 
 data2 [label = 'emi_rate\n= emigration\nrate\n per group', shape = folder, fillcolor = gold]
 
